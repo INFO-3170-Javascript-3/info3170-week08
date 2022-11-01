677010f7ba4e33feb0fda552a905f9835872c928
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2022-11-01 09:17:49 -0400

    week08

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDLWdC5YXG_-p9CUYANOM7ka-4i9FNQ_M0",
+    authDomain: "info3170-week08.firebaseapp.com",
+    projectId: "info3170-week08",
+    storageBucket: "info3170-week08.appspot.com", 
+    messagingSenderId: "915425341136",
+    appId: "1:915425341136:web:92d1e7cb0e023202aff8d5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDLWdC5YXG_-p9CUYANOM7ka-4i9FNQ_M0",
+    authDomain: "info3170-week08.firebaseapp.com",
+    projectId: "info3170-week08",
+    storageBucket: "info3170-week08.appspot.com", 
+    messagingSenderId: "915425341136",
+    appId: "1:915425341136:web:92d1e7cb0e023202aff8d5"
+  }
 };
 
 /*
